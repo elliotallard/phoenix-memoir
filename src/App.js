04b65551e4c2f154fc92122007642b10043d31b3
Elliot Allard <eallard@andrew.cmu.edu>
2019-10-23 14:50:03 -0400

    working on font, unsucessfully. switching over to work on home page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import appRoutes from "./shared/appRoutes";
 
-function App() {
+// import NavBar from "./containers/NavBar/NavBar";
+// import Footer from "./containers/Footer/Footer";
+// import HomePage from "./containers/HomePage/HomePage";
+// import CharactersPage from "./containers/CharactersPage/CharactersPage";
+// import CharacterPage from "./containers/CharactersPage/CharacterPage/CharacterPage";
+import "./App.css";
+
+const app = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+
+      <div className="MainContent">
+        <Switch>
+          <Route exact path={appRoutes.home}>
+            <HomePage />
+          </Route>
+          <Route exact path={appRoutes.characters}>
+            <CharactersPage />
+          </Route>
+          <Route exact path={appRoutes.character}>
+            <CharacterPage />
+          </Route>
+          <Redirect to={appRoutes.home} />
+        </Switch>
+      </div>
+
+      <Footer />
     </div>
   );
-}
+};
 
-export default App;
+export default app;
