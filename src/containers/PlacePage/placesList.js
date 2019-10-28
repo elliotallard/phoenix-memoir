78a1031f2f138da0e93b71ed424b1ebe53ec3d4f
Elliot Allard <eallard@andrew.cmu.edu>
2019-10-28 16:07:13 -0400

    just added images. getting ready to work on nav

---
+++ src/containers/PlacePage/placesList.js
@@ -0,0 +1,28 @@
+import CharK2SO from "../../assets/imgs/char-k2so.jpg";
+import CharC3PO from "../../assets/imgs/char-c3po.jpg";
+import CharRey from "../../assets/imgs/char-rey.jpg";
+import CharFinn from "../../assets/imgs/char-finn.jpg";
+import CharLuke from "../../assets/imgs/char-luke.jpg";
+import CharR2D2 from "../../assets/imgs/char-r2d2.jpg";
+import CharYoda from "../../assets/imgs/char-yoda.jpg";
+import CharChewbacca from "../../assets/imgs/char-chewbacca.jpg";
+import CharObiWan from "../../assets/imgs/char-obiwan.jpg";
+import CharLeia from "../../assets/imgs/char-leia.jpg";
+import CharHanSolo from "../../assets/imgs/char-han-solo.jpg";
+import CharDarthVader from "../../assets/imgs/char-darth-vader.jpg";
+
+export default [
+  {
+    id: "k2so",
+    rank: 1200,
+    name: "K-2SO",
+    photo: CharK2SO,
+    keyline: "I find that argument vague and unconvincing.",
+    who: [
+      "The lanky, cranky ex-Imperial security droid who helps the Rebels break into the Imperial compound in ‘Rogue One’. Think C-3PO with a bad attitude and verbal diarrhoea (though with the same clipped British accent)."
+    ],
+    why: [
+      "Because he’s the first Star Wars sidekick who’s genuinely funny – K-2’s way with a dry, often unintentional putdown is sardonically hilarious. The design is great, too – he’s all hulking and ungainly, like an overgrown metal teenager."
+    ]
+  },
+];
--- src/containers/PlaceScroll/PlaceScroll.js
+++
@@ -1,7 +0,0 @@
-{/* with much help from https://www.magictoolbox.com/magicscroll/integration/ */}
-
-
-import React from "react";
-import Cactus from "../../assets/cactus.png";
-// import YouTube from "react-youtube";
-import "./PlaceScroll.css";
