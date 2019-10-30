86c91bad504005b5aaa5930729cc4ae6bbdf3f56
Elliot Allard <eallard@andrew.cmu.edu>
2019-10-30 19:35:16 -0400

    formatted about page

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -12,22 +12,30 @@ import appRoutes from "../../shared/appRoutes";
 const aboutPage = () => {
 
 	return (
-      
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
 
-		  <p className="IntroText">
-        <b>October 30th, 2019 - </b>
-        <br/>
-        A nostalgia box, to remember the <i>hottest</i> summer. 
-        <br/><br/>
-        Miss, love, and thank you all - 
-        <br/>
-          
-          M., S., J., M., C., H., J., A., T., K., J., D., C., G.,
-          R., J., P., T., R., T., O., D., D., J., E., D., A., A., R.,
-          J., & P.
-          
-          ❤️ 
-      </p>
+            <br /><br />
+
+      		  <p className="IntroText">
+              <b>October 30th, 2019 - </b>
+              <br/>
+              A nostalgia box, to remember the <i>hottest</i> summer. 
+              <br/><br/>
+              Miss, love, and thank you all - 
+              <br/>
+                
+                M., S., J., M., C., H., J., A., T., K., J., D., C., G.,
+                R., J., P., T., R., T., O., D., D., J., E., D., A., A., R.,
+                J., & P.
+                
+                ❤️ 
+            </p>
+
+          </div>
+        </div>
+      </div>
        );
 }
 
